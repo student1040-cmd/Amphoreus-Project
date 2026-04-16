903f3858444cc57efaa8593c614297a88e7684dd
Олександра <student1040@otfk.ukr.education>
2026-04-16 16:12:26 +0300

    Fix: add allowedHosts to vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  plugins: [react()],
+  server: {
+    allowedHosts: [
+      'laba-2-frontend.onrender.com' // Додаємо твій домен сюди
+    ],
+    host: true,
+    port: 5173
+  }
 })
